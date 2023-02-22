880481d05f1944722aa7ef2fadf6416a2690e5c6
Richard Russell <95770274+richard-russell@users.noreply.github.com>
2023-02-22 18:05:10 +0000

    Remove ami data source

--- main.tf
+++ main.tf
@@ -5,26 +5,9 @@ provider "aws" {
 variable "aws_region" { default = "eu-west-2" } # London
 
 /*
-# Non-compliant config - data source in root module
-data "aws_ami" "ubuntu" {
-    most_recent = true
-
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-    }
-
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-
-    owners = ["099720109477"] # Canonical
-}
-
 # Non-compliant config - Resource created from root module
 resource "aws_instance" "bad_ubuntu" {
-  ami               = data.aws_ami.ubuntu.id
+  ami               = ami-0ac10f53765369588
   instance_type     = "t2.micro"
  
   tags = {
